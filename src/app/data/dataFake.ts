0ac544c3747bf7f421ae5db3610568226cbd2c52
Tiago Pace <tiagopace.dev@gmail.com>
2025-04-04 16:59:24 -0300

    feat: travel blog

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,48 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Explore the Beauty of the Swiss Alps',
+    description:
+      'Discover breathtaking views and serene landscapes in Switzerland.',
+    photoCover:
+      'https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDF8fHRyYXZlbHxlbnwwfHx8fDE2ODAxNjYwNzI&ixlib=rb-1.2.1&q=80&w=1200',
+  },
+  {
+    id: '2',
+    title: 'Top 10 Beaches to Visit in the Maldives',
+    description:
+      'Relax on pristine beaches and enjoy crystal-clear waters in the Maldives.',
+    photoCover:
+      'https://plus.unsplash.com/premium_photo-1683134073814-ebceb033971f?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YmVhY2hlc3xlbnwwfDB8MHx8fDA%3D',
+  },
+  {
+    id: '3',
+    title: 'Experience the Northern Lights in Iceland',
+    description: 'Witness the breathtaking auroras in the skies of Iceland.',
+    photoCover:
+      'https://images.unsplash.com/41/LGhxuAbT5Wop4JYcrMpV_IMG_3808.jpg?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8aWNlbGFuZHxlbnwwfDB8MHx8fDA%3D',
+  },
+  {
+    id: '4',
+    title: 'A Journey Through the Streets of Kyoto, Japan',
+    description: "Explore the rich culture and history of Kyoto's streets.",
+    photoCover:
+      'https://images.unsplash.com/photo-1545569341-9eb8b30979d9?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8a3lvdG98ZW58MHwwfDB8fHww',
+  },
+  {
+    id: '5',
+    title: 'Safari Adventures in Serengeti, Tanzania',
+    description:
+      'Embark on a thrilling safari and witness the majestic wildlife of Serengeti.',
+    photoCover:
+      'https://plus.unsplash.com/premium_photo-1661859352097-855a040f944c?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8c2FmYXJpfGVufDB8MHwwfHx8MA%3D%3D',
+  },
+  {
+    id: '6',
+    title: 'Exploring the Fjords of Norway',
+    description:
+      'Sail through the breathtaking fjords and enjoy the natural beauty of Norway.',
+    photoCover:
+      'https://plus.unsplash.com/premium_photo-1668017178979-9e341bfaa464?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8RmpvcmRzJTIwTm9yd2F5fGVufDB8MHwwfHx8MA%3D%3D',
+  },
+];
